df2536fcecb08c6f89fbfcc814ff95161c74e813
onomeh <onwuanibeonome@gmail.com>
2022-06-10 21:08:15 +0000

    Use redux in react components

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Categories = () => (
-  <button type="button">Check Status</button>
-);
+const Categories = () => {
+  const categories = useSelector((state) => state.categories);
+  return (
+    <div>
+      <h1>{categories}</h1>
+      <button type="button">Check Status</button>
+    </div>
+  );
+};
 
 export default Categories;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
+import { Provider } from 'react-redux';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function

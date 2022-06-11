28624d8f4c3d09966b245d15f3e6111dc9ea7dc5
onomeh <onwuanibeonome@gmail.com>
2022-06-11 09:33:50 +0000

    connect to API

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,33 +1,50 @@
+import axios from 'axios';
+
 const ADD_BOOK = 'ADD_BOOK';
 const DELETE_BOOK = 'DELETE_BOOK';
+const GET_BOOK = 'GET_BOOK';
 
 // Action creator
 export const addBook = (newBookToAdd) => ({ type: ADD_BOOK, newBookToAdd });
 export const deleteBook = (id) => ({ type: DELETE_BOOK, id });
+export const getBook = (payload) => ({ type: GET_BOOK, payload });
 
-const initialState = [
-  {
-    id: 1,
-    author: 'Author 1',
-    title: 'Title 1',
-    category: 'Category 1',
-  },
-  {
-    id: 2,
-    author: 'Author 2',
-    title: 'Title 2',
-    category: 'Category 2',
-  },
-];
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/N14gCXiKekJEuGCIphaC/books';
+
+export const fetchBook = () => async (dispatch) => {
+  const response = await axios.get(url);
+  const data = await response.data;
+  const books = [];
+  Object.entries(data).forEach((book) => {
+    books.push({
+      item_id: book[0],
+      ...book[1][0],
+    });
+  });
+  dispatch(getBook(books));
+};
+
+export const postBook = (newBookToAdd) => async (dispatch) => {
+  await axios.post(url, newBookToAdd);
+  dispatch(addBook(newBookToAdd));
+};
+
+export const removeBook = (id) => async (dispatch) => {
+  await axios.delete(`${url}/${id}`);
+  dispatch(deleteBook(id));
+};
 
 // Reducer logic
-const bookReducer = (state = initialState, action) => {
+const bookReducer = (state = [], action) => {
   if (action.type === ADD_BOOK) {
     return [...state, action.newBookToAdd];
   }
 
   if (action.type === DELETE_BOOK) {
-    return [...state].filter((book) => book.id !== action.id);
+    return [...state].filter((book) => book.item_id !== action.id);
+  }
+  if (action.type === GET_BOOK) {
+    return action.payload;
   }
   return state;
 };
